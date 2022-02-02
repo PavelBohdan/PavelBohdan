0aac7732745d7e197eaa1ae2bbd404b2c16cd202
PavelBohdan <lllhypok87@gmail.com>
2022-02-02 13:34:57 +0300

    conftest changed

--- test_po/conftest.py
+++ test_po/conftest.py
@@ -4,7 +4,13 @@ import pytest
 
 
 @pytest.fixture(scope='session')
-def driver():
-    driver = webdriver.Chrome()
+def browser():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.maximize_window()
+    driver.implicitly_wait(5)
     yield driver
     driver.quit()
