4e62d2bd6287addf44e24a7bda4fe8ec0cfb7184
PavelBohdan <91595873+PavelBohdan@users.noreply.github.com>
2022-02-02 13:10:35 +0300

    Delete test_jenkins_29.py

--- test_jenkins_29.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-
-def test_jenkins_plugin(browser):
-    browser.get('https://plugins.jenkins.io/shiningpanda')
-    title_element = browser.find_element(By.XPATH, '//h1[@class="title"]')
-    assert title_element.text == 'ShiningPanda'
